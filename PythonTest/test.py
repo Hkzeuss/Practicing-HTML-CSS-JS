bf7926f39a1b28fe4432a07ed72f189e584aa263
Hkzeuss <hoangtientrungkien2k3@gmail.com>
2023-08-30 17:31:55 +0700

    update

---
+++ PythonTest/test.py
@@ -0,0 +1,13 @@
+age = int(input("Nhập tuổi bạn gái của bạn: "))
+
+if age >= 18:
+    if age <60:
+        print("Bạn gái của bạn đủ tuổi")
+
+    elif age >= 60:
+        print("Bạn là người cao tuổi")
+else:
+    print("bạn có thể đi tù.")
+    
+    
+    
